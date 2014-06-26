f510125cbbc7a33b63252e06b090e7938c6eef3a
Luke Imhoff <Kronic.Deth@gmail.com>
2014-06-25 19:45:40 -0500

    Issus.CLI.process/1

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -5,7 +5,9 @@ defmodule Issues.CLI do
   that end up generating a table of the last _n_ issues in a github project
   """
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
   @doc """
   `argv` can be -h or --help, which returns :help.
@@ -31,5 +33,16 @@ defmodule Issues.CLI do
         -> { user, project, @default_count }
       _ -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+  
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
